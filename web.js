e0d3da420d119cc916b2ebaf7f386df73dbcf93e
Derek VerLee <dlverlee@mtu.edu>
2012-03-24 18:16:59 -0400

    Demonstrate ability to read env

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var text = 'Hello World!\n';
+    text += 'I am running with\n';
+    text += process.env.HOME;
+    response.send(text);
 });
 
 
