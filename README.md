e426b92b437b917318f3beb7dc122acdc8d22abf
Derek VerLee <dlverlee@mtu.edu>
2012-03-24 18:59:55 -0400

    Start with "Blog rolling" tutorial
    http://howtonode.org/af136c8ce966618cc0857dbc5e5da01e9d4d87d5/express-mongodb
    http://howtonode.org/express-mongodb

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Tutorials Consumed
 ------------------
 [Heroku - Node.js getting started tutorial](http://devcenter.heroku.com/articles/nodejs)
 
+[Blog rolling with mongoDB, express and Node.js](http://howtonode.org/af136c8ce966618cc0857dbc5e5da01e9d4d87d5/express-mongodb)
--- web.js
+++ web.js
@@ -1,8 +1,13 @@
+/* Explicitly copied or adopted from various sources:
+   see README.md */
+
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = module.exports = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+app.get('/hello', function(request, response) {
     var text = 'Hello World!\n';
     text += 'I am running with\n';
     text += process.env.HOME;
@@ -10,9 +15,37 @@ app.get('/', function(request, response) {
 });
 
 
-app.get('/egg.html', function(request, response) {
+app.get('/egg', function(request, response) {
     response.send('happy easter');
 });
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
+
+app.listen(3000);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
