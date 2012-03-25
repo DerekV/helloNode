497ed0ce666bfb2a9b23bceeab2558e4762e9e87
Derek VerLee <dlverlee@mtu.edu>
2012-03-24 22:19:49 -0400

    Should only listen once

--- web.js
+++ web.js
@@ -66,8 +66,6 @@ app.post('/blog/new', function(req, res){
     });
 });
 
-app.listen(3000);
-
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
